90a78340b7ffdc526cc07d19c33a2e2b73060625
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-09 22:42:03 -0300

    add pylint

--- productos/admin.py
+++ productos/admin.py
@@ -3,7 +3,11 @@ from .models import Producto
 from .models import Categoria
 from .models import Cliente
 
-
+class CategoriaAdmin(admin.ModelAdmin):
+    exclude = ('fecha_creacion', 'fecha_modificacion')
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre', 'descripcion')
+    
 # Register your models here.
 
 admin.site.register(Producto)
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Producto
 
 # Create your views here.
 def index(request):
+    productos = Producto.objects.all()
+    
     return HttpResponse("Hello, world. You're at the productos index.")
